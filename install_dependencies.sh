deb0948218c2605b8712e4eac50514550b8c5507
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2024-08-24 14:01:29 +0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,6 +3,4 @@ curl -fsSL https://get.docker.com/ | sh
 sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
-docker pull lordagam/joseph-agam-project-devops_node:22
-docker pull lordagam/joseph-agam-project-devops_php:22
-docker pull lordagam/joseph-agam-project-devops_mysql:8.0.28
+docker pull talsa/tal_ex2_exam:latest
