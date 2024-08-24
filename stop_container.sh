c9c2f95a48aeb04cad44fab2ee6f2ffb27edcf44
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2024-08-24 12:06:52 +0300

    Create stop_container.sh

---
+++ stop_container.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+docker stop lordagam/joseph-agam-project-devops_node:latest
+docker stop lordagam/joseph-agam-project-devops_php:latest
+docker stop lordagam/joseph-agam-project-devops_mysql:8.0.28
+
+docker rm lordagam/joseph-agam-project-devops_node:latest
+docker rm lordagam/joseph-agam-project-devops_php:latest
+docker rm lordagam/joseph-agam-project-devops_mysql:8.0.28
+
+docker rmi -f lordagam/joseph-agam-project-devops_node:latest
+docker rmi -f lordagam/joseph-agam-project-devops_php:latest
+docker rmi -f lordagam/joseph-agam-project-devops_mysql:8.0.28
