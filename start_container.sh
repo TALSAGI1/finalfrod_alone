929a7bd4bd59bf1024a3d12d7357a5040ee962e3
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2024-08-24 14:02:20 +0300

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
-docker run -itd --name joseph-agam-project-devops_node -p 3000:3000 lordagam/joseph-agam-project-devops_node:22
-docker run -itd --name joseph-agam-project-devops_php -p 80:80 lordagam/joseph-agam-project-devops_php:22
-docker run -itd --name joseph-agam-project-devops_mysql -p 3306:3306 -e MYSQL_DATABASE=super-app -e MYSQL_ROOT_PASSWORD=password mysql:8.0.28
+docker run -itd --name tal_ex2_exam_node -p 3000:3000 talsa/tal_ex2_exam:latest
+docker run -itd --name tal_ex2_exam_php -p 80:80 talsa/tal_ex2_exam:latest
+docker run -itd --name tal_ex2_exam_mysql -p 3306:3306 -e MYSQL_DATABASE=super-app -e MYSQL_ROOT_PASSWORD=password talsa/tal_ex2_exam:latest
